42ab12919ac8af856a3f9675c77377bd860ff6db
Tamaghna Chakraborty <tamdbest@gmail.com>
2022-07-24 11:24:18 +0530

    end of part 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
+using AutoMapper;
+using Application.Core;
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config){
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(options=>{
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(options=>{
+                options.AddPolicy("CorsPolicy",policy=>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            return services;
+        }
+    }
+}
